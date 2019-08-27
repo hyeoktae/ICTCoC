87e66e9503c74c9ce7c197a99e0cf075887c3192
hyeoktae kwon <kht_0218@hanmail.net>
2019-08-28 01:01:26 +0900

    feat: tass - Alamofire, SnapKit, Kingfisher Install

---
+++ iOS2and3/Pods/Target Support Files/Pods-iOS2and3/Pods-iOS2and3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS2and3 : NSObject
+@end
+@implementation PodsDummy_Pods_iOS2and3
+@end
---
+++ iOS2and3/iOS2and3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS2and3
+//
+//  Created by hyeoktae kwon on 2019/08/28.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
